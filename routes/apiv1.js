1833a3312b65cb69cb2a19eb6d713ff6aa4fff52
Shivam Solanki <Shivam.Raj.Solanki@ibm.com>
2019-12-11 15:47:41 -0600

    updating routes/apiv1 to pass fvt

--- routes/apiv1.js
+++ routes/apiv1.js
@@ -7,7 +7,7 @@ var request = REQUEST.defaults( {
     strictSSL: false
 });
 
-var OPENWEATHERURL = "http://api.openweathermap.org/data/2.5/weather?appid=6b7b471967dd0851d0010cdecf28f829&units=imperial";
+var OPENWEATHERURL = "https://api.openweathermap.org/data/2.5/weather?appid=6b7b471967dd0851d0010cdecf28f829&units=imperial";
 
 exports.getWeather = function(req, res) {
 	var zip = req.query.zip;
@@ -39,7 +39,6 @@ exports.getWeather = function(req, res) {
 };
 router.get('/getWeather', exports.getWeather);
 
-/*
 exports.getWeather2 = function(req, res) {
 	var zip = req.query.zip;
 	if( (zip === null) || (typeof(zip) === 'undefined') ) {
@@ -69,6 +68,5 @@ exports.getWeather2 = function(req, res) {
 
 };
 router.get('/getWeather2', exports.getWeather2);
-*/
 
 exports.router = router;
